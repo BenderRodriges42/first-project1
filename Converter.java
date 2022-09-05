75b8727862b24d49790c8a3a86796048af8a98d1
Vladimir <blender424242@yandex.ru>
2022-09-05 11:06:42 +0300

    Добавил коммент в Convertere и удалил метод мэйн в Прктикуме

---
+++ Converter.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+
+public class testo8 {
+    public static void main(String[] args) {
+ class Converter {
+    double rateUSD;
+    double rateEUR;
+    double rateJPY;
+
+    Converter(double usd, double eur, double jpy) {
+        rateUSD = usd;
+        rateEUR = eur;
+        rateJPY = jpy;
+    }
+
+    void convert(double rubles, int currency) {
+        if (currency == 1) {
+            System.out.println("Ваши сбережения в долларах: " + rubles / rateUSD);
+        } else if (currency == 2) {
+            System.out.println("Ваши сбережения в евро: " + rubles / rateEUR);
+        } else if (currency == 3) {
+            System.out.println("Ваши сбережения в иенах: " + rubles / rateJPY);
+        } else {
+            System.out.println("Неизвестная валюта");
+        }
+    }
+}
+    }}/// Добавил коммент 
+ 
